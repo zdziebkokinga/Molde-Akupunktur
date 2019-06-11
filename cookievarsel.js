bd5a14569e37fca3fa0ab3699f241551ee1b9af4
Kinga Zdziebko <zdziebkokinga@gmail.com>
2019-06-11 20:11:19 +0200

    1. oppdatering

---
+++ cookievarsel.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#383b75"
+    },
+    "button": {
+      "background": "#f1d600"
+    }
+  },
+  "theme": "classic",
+  "content": {
+    "message": "Nettsiden bruker informasjonskapsler (Cookies) for en optimal opplevelse.",
+    "dismiss": "Godta",
+    "link": "Mer informasjon",
+    "href": "/personvern.html"
+  }
+})});
